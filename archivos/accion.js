23636838cd61e994c6a92ae914b4d59023ec0bd7
Automecanic <albertofernandez@Tremenda.local>
2020-08-04 18:20:54 +0200

    septima

---
+++ archivos/accion.js
@@ -0,0 +1,9 @@
+/* recoge el evento del  documento cuanto esta cargado y ejecuta la  funcion  ecogiendo el evento */
+document.addEventListener("DOMContentLoaded",function(event) {
+
+
+console.log("carga completa");
+
+
+
+});

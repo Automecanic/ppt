7bcd82c1a1e03bea14065093470e244904605484
Automecanic <albertofernandez@Tremenda.local>
2020-08-05 11:00:36 +0200

    octava

--- archivos/accion.js
+++ archivos/accion.js
@@ -1,9 +1,15 @@
 /* recoge el evento del  documento cuanto esta cargado y ejecuta la  funcion  ecogiendo el evento */
-document.addEventListener("DOMContentLoaded",function(event) {
+/*document.addEventListener("DOMContentLoaded",function(event) {*/
 
-
-console.log("carga completa");
-
-
+   const btnToggle= document.querySelector('.toggle-btn');
+ btnToggle.addEventListener('click', function(){
+    var control=document.getElementById('sidebar').classList.toggle('active');
+     if (control){
+         document.getElementById('sp').innerHTML="x";
+     }
+     else {
+        document.getElementById('sp').innerHTML='&#9776;';
+     }
 
 });
+/*});*/

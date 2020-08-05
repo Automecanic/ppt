d35e59d599912ae5ee76495dd40b7b8dc1af1c6b
Automecanic <albertofernandez@Tremenda.local>
2020-08-05 11:07:01 +0200

    octava dos

--- archivos/accion.js
+++ archivos/accion.js
@@ -5,6 +5,7 @@
  btnToggle.addEventListener('click', function(){
     var control=document.getElementById('sidebar').classList.toggle('active');
      if (control){
+         /*controla  los elementos  de dentro del Tag span y pone una x si esta extendido*/
          document.getElementById('sp').innerHTML="x";
      }
      else {
